8963aa0ec1ab2f0b79d604a45278d0421992ff9c
Nils <its.just.nils@gmail.com>
2018-01-13 10:22:08 +0100

    added player movement direction based on camera angle

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CCore.CubeWorlds.Cameras;
 using CCore.CubeWorlds.GameInput;
 using UnityEngine;
 
@@ -7,45 +8,73 @@ namespace CCore.CubeWorlds.Players
     [RequireComponent(typeof(Player))]
     public class PlayerMovement : MonoBehaviour, IPlayerEnabler
     {
-		[SerializeField] private float movementSpeed;
-
 		private Player player;
+
+		private new Rigidbody rigidbody;
+
+		private Vector3 cameraDirection;
 
 		private void Start()
 		{
 			player = GetComponent<Player>();
+
+			rigidbody = GetComponent<Rigidbody>();
+		}
+
+		private void Update()
+		{
+			UpdatePosition();
 		}
 
         private void OnPlayerInput(object sender, PlayerInputArgs e)
         {
 			if (e.inputState != InputState.Hold)
 			{
+				cameraDirection = Vector3.zero;
+
 				return;
 			}
+
+			Transform cameraTransform = player.CameraSlot.Camera.transform;
 
             switch (e.playerInputType)
 			{
 				case PlayerInputType.MoveLeft:
-					MoveCharacter(-transform.right, player.CameraSlot.Camera.transform);
+					cameraDirection = -cameraTransform.right;
 					break;
 				
 				case PlayerInputType.MoveRight:
-					MoveCharacter(transform.right, player.CameraSlot.Camera.transform);
+					cameraDirection = cameraTransform.right;
 					break;
 				
 				case PlayerInputType.MoveBackward:
-					MoveCharacter(-transform.forward, transform);
+					cameraDirection = -cameraTransform.forward;
 					break;
 				
 				case PlayerInputType.MoveForward:
-					MoveCharacter(transform.forward, transform);
+					cameraDirection = cameraTransform.forward;
 					break;
 			}
         }
 
-		private void MoveCharacter(Vector3 direction, Transform relativeTo)
+		private void UpdatePosition()
 		{
-			transform.Translate(direction * movementSpeed, Space.World);
+			Vector3 force = GetMovementForce();
+
+			rigidbody.AddRelativeForce(force, ForceMode.Force);
+		}
+
+		private Vector3 GetMovementForce()
+		{
+			Vector3 localCameraDirection = transform.worldToLocalMatrix.MultiplyVector(cameraDirection);
+
+			localCameraDirection.y = 0f;
+
+			Vector3 normalizedVector = Vector3.Normalize(localCameraDirection);
+
+			// TODO: Add strength multiplier
+
+			return normalizedVector;
 		}
 
         public void OnPlayerEnabled()
